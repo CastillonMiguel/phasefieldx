a193145788ad05e318e7cfbc7f1c02883bce4211
CastillonMiguel <miguel.castillon.demiguel@alumnos.upm.es>
2025-06-22 16:20:48 +0200

    Reformat test_gmsh_geo_file_processing.py by commenting out code

--- test/test_gmsh_geo_file_processing.py
+++ test/test_gmsh_geo_file_processing.py
@@ -1,21 +1,21 @@
-import os
-import pytest
-import gmsh
-import pyvista as pv
+# import os
+# import pytest
+# import gmsh
+# import pyvista as pv
 
-# Define the folder containing the .geo files
-FOLDER = "geo_files"
-GEO_FILES = ["file_2d.geo", "file_3d.geo"]  # Replace with your actual .geo file names
-VTU_FILES = [os.path.join(FOLDER, "output_mesh_for_view_1.vtk"),
-             os.path.join(FOLDER, "output_mesh_for_view_2.vtk")]
+# # Define the folder containing the .geo files
+# FOLDER = "geo_files"
+# GEO_FILES = ["file_2d.geo", "file_3d.geo"]  # Replace with your actual .geo file names
+# VTU_FILES = [os.path.join(FOLDER, "output_mesh_for_view_1.vtk"),
+#              os.path.join(FOLDER, "output_mesh_for_view_2.vtk")]
 
 
-@pytest.fixture(scope="module", autouse=True)
-def setup_gmsh():
-    """Set up GMSH for tests."""
-    gmsh.initialize()
-    yield
-    gmsh.finalize()
+# @pytest.fixture(scope="module", autouse=True)
+# def setup_gmsh():
+#     """Set up GMSH for tests."""
+#     gmsh.initialize()
+#     yield
+#     gmsh.finalize()
 
 
 # @pytest.mark.parametrize("geo_file, vtu_file", zip(GEO_FILES, VTU_FILES))
