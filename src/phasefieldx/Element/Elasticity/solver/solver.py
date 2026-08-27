3d8cd9ae27cc8860eaa49106de18baa2d4ca2ee3
CastillonMiguel <miguel.castillon.demiguel@alumnos.upm.es>
2026-08-27 09:35:13 +0200

    Add log_header function to solvers for enhanced logging information

--- src/phasefieldx/Element/Elasticity/solver/solver.py
+++ src/phasefieldx/Element/Elasticity/solver/solver.py
@@ -13,7 +13,7 @@ import ufl
 from mpi4py import MPI
 
 from phasefieldx.files import prepare_simulation, append_results_to_file
-from phasefieldx.Logger.library_versions import set_logger, log_library_versions, log_system_info, log_end_analysis, log_model_information
+from phasefieldx.Logger.library_versions import set_logger, log_library_versions, log_system_info, log_end_analysis, log_model_information, log_header
 
 from phasefieldx.Materials.elastic_isotropic import epsilon, sigma, psi
 from phasefieldx.Reactions import calculate_reaction_forces
@@ -99,6 +99,7 @@ def solve(Data,
     if rank == 0:
         prepare_simulation(path, result_folder_name)
         logger = set_logger(result_folder_name)
+        log_header(logger)
         log_system_info(logger)  # log system information
         log_library_versions(logger)  # log Library versions
         Data.save_log_info(logger)  # log Simulation input data
